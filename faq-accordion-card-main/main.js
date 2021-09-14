3df1cd150b07a43abd26a2f3d99324a1da95785b
Jos02378 <57058570+Jos02378@users.noreply.github.com>
2021-09-14 22:58:00 +0700

    update accordion styles

--- faq-accordion-card-main/main.js
+++ faq-accordion-card-main/main.js
@@ -1,22 +1,18 @@
 const accordions = document.querySelectorAll('.container__box');
 
 accordions.forEach((accordion) => {
-    accordion.addEventListener('click', () => {
-        if (accordion.classList.contains('active')) {
-            accordion.classList.remove('active');
-        } else {
-            let currentAccordion = document.querySelector(
-                '.container__box.active'
-            );
+  accordion.addEventListener('click', () => {
+    if (accordion.classList.contains('active')) {
+      accordion.classList.remove('active');
+    } else {
+      let currentAccordion = document.querySelector('.container__box.active');
 
-            if (currentAccordion) {
-                currentAccordion.classList.remove('active');
-                let currentLabel =
-                    currentAccordion.querySelector('.container__label');
-                currentLabel.style.fontWeight = '400';
-            }
+      if (currentAccordion) {
+        currentAccordion.classList.remove('active');
+        let currentLabel = currentAccordion.querySelector('.container__label');
+      }
 
-            accordion.classList.add('active');
-        }
-    });
+      accordion.classList.add('active');
+    }
+  });
 });
